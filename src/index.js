8e5ffa709f8ebcae4bd2828975f4050a2103e561
nmo83 <125308408+nmo83@users.noreply.github.com>
2023-03-23 21:44:45 +0100

    update to lesson 234

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots }  from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World!</h1>
-    <App />
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
